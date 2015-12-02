9635dd23675917b168ef9415839e72538ae165e4
Bartok <bartok@Bartoks-MacBook-Pro.local>
2015-12-01 20:21:27 -0800

    Add demo folder

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Final Project for UW Continuing Education Course
 
 The root folder houses the ruby server and controller.
 
-The sites folder is the front end for hitting the server api and displaying the information.
+The demo folder is the front end for hitting the server api and displaying the information.
---
+++ demo/app/scripts/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+  function createFlight() {
+    flight_id = Math.random().toString(36).replace(/[^a-z]+/g,'').slice(2, 4).toUpperCase() + Math.random().toString().slice(2,5);
+    $.ajax({
+      method: 'GET',
+      url: "http://localhost:3000/entry?flight=" + flight_id
+    })
+    .done(function(msg) {
+      console.log('saved' + msg);
+    });
+  }
+  createFlight();
+
+  function getFlights() {
+    $.ajax({
+      method: 'GET',
+      url: "http://localhost:3000/tracking_info"
+    })
+    .done(function(data) {
+      console.log(data);
+      $('.marketing .col-lg-6').html(data);
+    });
+  }
+  getFlights();
+
+  (function loop_flight() {
+    var rand = Math.floor(Math.random() * (45000 - 25000) + 25000);
+    console.log(rand);
+    setTimeout(function() {
+            createFlight();
+            loop_flight();
+    }, rand);
+  }());
+
+  (function loop_data() {
+    setTimeout(function() {
+      console.log('getdata');
+      getFlights();
+      loop_data();
+    }, 5000);
+  }());
+
+});
